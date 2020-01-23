7188c6635f379fa3522f85d7ec53d32d0061daa2
Ali Solanki <alisolanki123@gmail.com>
2020-01-24 01:20:54 +0530

    Frontend 1.0.3

---
+++ app/src/main/java/com/example/hack20/CalenderActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hack20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalenderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calender)
+    }
+}
--- app/src/main/java/com/example/hack20/UserActivity.kt
+++ app/src/main/java/com/example/hack20/UserActivity.kt
@@ -1,12 +1,25 @@
 package com.example.hack20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_user.*
 
 class UserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+
+        button_addactivity.setOnClickListener {
+            var intent = Intent(this, AddActivity::class.java)
+            startActivity(intent);
+        }
+
+        button_calender.setOnClickListener {
+            var intent = Intent(this, CalenderActivity::class.java)
+            startActivity(intent);
+        }
     }
 }
